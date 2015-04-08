48471de832200d7653d2f5f1e5563c7f4aa1e871
gman <gmhooray@naver.com>
2015-04-08 17:22:34 +0900

    [TASK] project init

---
+++ js/namespace.js
@@ -0,0 +1,11 @@
+/**
+ * @description Civil Project
+ * @namespace
+ * @name _g_
+ * @version 0.0.1
+ * @since April 09, 2015
+ * @copyright Copyright (c) 2015, G.fam
+ * @private
+ */
+
+var _g_ = _g_ || {};
