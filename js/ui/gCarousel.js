b79bc977de7ef53841cf915dc4e914bfa0a18fc0
gman <naver@naverui-MacBook-Air.local>
2015-04-14 11:59:58 +0900

    [TASK] scope modified

--- js/ui/gCarousel.js
+++ js/ui/gCarousel.js
@@ -1,16 +1,17 @@
+(function(){
+    _g_.ui.carousel = function(){this.$init.apply(this,arguments)};
 
-_g_.ui.carousel = function(){this.$init.apply(this,arguments)};
+    _g_.ui.carousel.prototype = {
+        $init:	function(){
+            $('.item').height(window.innerHeight);
+            $('.carousel').carousel({
+                interval: 2000
+            })
+            $('#startApp').on('click',function(){
+                location.href= "http://m.naver.com";
+            });
+        }
+    }
 
-_g_.ui.carousel.prototype = {
-    $init:	function(){
-		$('.item').height(window.innerHeight);
-		$('.carousel').carousel({
-			interval: 2000
-		})
-		$('#startApp').on('click',function(){
-			location.href= "http://m.naver.com";
-		});
-	}
-}
-
-new _g_.ui.carousel();
+    new _g_.ui.carousel();
+})
